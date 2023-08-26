870f20f9dda221f189f0c28ed1479e0518f49313
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-08-26 12:58:29 +0530

    start Quora clone project

---
+++ src/Components/Quora.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './navbar';
+function Quora() {
+  return (
+    <div>
+      <h1>Quora</h1>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Quora;
---
+++ src/Components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className='navbar'>
+        
+    </div>
+  )
+}
+
+export default navbar;
