21943c6290253d2f7866022d70c9b0795cd5baed
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-08-27 03:12:02 +0530

    update feed

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Feed.css'
 import QuoraDefaultBox from './QuoraDefaultBox';
+import QuoraFeedbox from './QuoraFeedbox';
 function Feed() {
   return (
     <div className='Feed'>
       <QuoraDefaultBox/>
+      <QuoraFeedbox/>
+      <QuoraFeedbox />
+      <QuoraFeedbox/>
+      <QuoraFeedbox/>
     </div>
   )
 }
