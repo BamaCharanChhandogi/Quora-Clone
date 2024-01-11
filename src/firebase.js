ca8452b72f80734aadc373adc4eff579778061df
Bama Charan Chhandogi <b.c.chhandogi@gmail.com>
2024-01-11 13:27:40 +0530

    update database

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE}`,
-  messagingSenderId: `${process.env.REACT_APP_MSI}`,
-  appId: `${process.env.REACT_APP_APP_ID}`,
+  apiKey: "AIzaSyAWbDm9oAZAkhSAFdYYaVrJZ7g5UT9kvWU",
+  authDomain: "quora-clone-64408.firebaseapp.com",
+  projectId: "quora-clone-64408",
+  storageBucket: "quora-clone-64408.appspot.com",
+  messagingSenderId: "729548785172",
+  appId: "1:729548785172:web:4ff0a701d52e666a300901",
+  measurementId: "G-V6MRE9H639"
 };
 
 console.log(firebaseConfig);
